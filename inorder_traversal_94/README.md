514ef14c38283989e9725575b4d25973852a7161
xiaobinfeng <rgfxb@qq.com>
2022-05-12 18:41:46 +0800

    -info=add 94

---
+++ inorder_traversal_94/README.md
@@ -0,0 +1,2 @@
+指针参考文章
+https://blog.csdn.net/u013792921/article/details/84565336
---
+++ inorder_traversal_94/solution_test.go
@@ -0,0 +1,39 @@
+package inorder_traversal_94
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+// 递归左中右
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func inorder(root *TreeNode, res *[]int) {
+	if root == nil {
+		return
+	}
+	inorder(root.Left, res)
+	*res = append(*res, root.Val)
+	inorder(root.Right, res)
+}
+
+func inorderTraversal(root *TreeNode) []int {
+	var res []int
+	inorder(root, &res)
+	return res
+}
