25c768e7a7394a8c3f32c210c9e46d07a4b8e850
xiaobinfeng <rgfxb@qq.com>
2022-05-14 23:17:55 +0800

    info=add 144

---
+++ preorder_traversal_144/solution_test.go
@@ -0,0 +1,34 @@
+package preorder_traversal_144
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func preOrder(root *TreeNode, arr *[]int) {
+	if root == nil {
+		return
+	}
+	*arr = append(*arr, root.Val)
+	preOrder(root.Left, arr)
+	preOrder(root.Right, arr)
+}
+
+func preorderTraversal(root *TreeNode) []int {
+	var arr []int
+	if root == nil {
+		return arr
+	}
+	preOrder(root, &arr)
+	return arr
+}
