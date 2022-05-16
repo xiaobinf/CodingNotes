9863a8a6f79c49bbf0bf771179e18fa332b2bb88
xiaobinfeng <rgfxb@qq.com>
2022-05-16 13:13:16 +0800

    info=add 561

---
+++ array_pair_sum_561/solution_test.go
@@ -0,0 +1,20 @@
+package array_pair_sum_561
+
+import (
+  "fmt"
+  "sort"
+  "testing"
+)
+
+func arrayPairSum(nums []int) int {
+  sort.Ints(nums)
+  sum := 0
+  for i := 0; i < len(nums); i = i + 2 {
+    sum += nums[i]
+  }
+  return sum
+}
+
+func TestName(t *testing.T) {
+  fmt.Println(arrayPairSum([]int{1, 3, 5, 4, 2, 9}))
+}
