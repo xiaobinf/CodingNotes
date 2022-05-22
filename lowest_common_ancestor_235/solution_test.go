35d6b36d82c61281b62d574833090a1553e4daf9
xiaobinfeng <rgfxb@qq.com>
2022-05-22 21:19:28 +0800

    info=add 235

---
+++ lowest_common_ancestor_235/solution_test.go
@@ -0,0 +1,27 @@
+package lowest_common_ancestor_235
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val   int
+ *     Left  *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+// 求二叉搜索树的公共祖先
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	if p.Val < root.Val && q.Val < root.Val {
+		return lowestCommonAncestor(root.Left, p, q)
+	} else if p.Val > root.Val && q.Val > root.Val {
+		return lowestCommonAncestor(root.Right, p, q)
+	} else {
+		return root
+	}
+}
