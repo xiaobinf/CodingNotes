ae5aba5a4f00f391e2207689edb34c0f635ac785
xiaobinfeng <rgfxb@qq.com>
2022-05-25 13:27:38 +0800

    info=add 83

---
+++ delete_duplicates_83/solution_test.go
@@ -0,0 +1,29 @@
+package delete_duplicates_83
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+type ListNode struct {
+  Val  int
+  Next *ListNode
+}
+
+func deleteDuplicates(head *ListNode) *ListNode {
+  if head == nil || head.Next == nil {
+    return head
+  }
+  node := head
+  for node.Next != nil {
+    if node.Next.Val == node.Val {
+      node.Next = node.Next.Next
+    } else {
+      node = node.Next
+    }
+  }
+  return head
+}
