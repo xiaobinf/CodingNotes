acf5dd096a33b0909cd31025fa6fa4d1fbd4e215
xiaobinfeng <rgfxb@qq.com>
2022-05-26 12:57:27 +0800

    info=add 21

---
+++ merge_two_lists_21/solution_test.go
@@ -0,0 +1,31 @@
+package merge_two_lists_21
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+// 本质上找到递归结束的条件 子问题的定义
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	if list1 == nil {
+		return list2
+	}
+	if list2 == nil {
+		return list1
+	}
+	if list1.Val < list2.Val {
+		list1.Next = mergeTwoLists(list1.Next, list2)
+		return list1
+	} else {
+		list2.Next = mergeTwoLists(list1, list2.Next)
+		return list2
+	}
+}
