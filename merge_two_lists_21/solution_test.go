566501553e7d462c47e156a198bb7e6ca5714f13
xiaobinfeng <rgfxb@qq.com>
2022-05-28 21:55:57 +0800

    info=add 21

--- merge_two_lists_21/solution_test.go
+++ merge_two_lists_21/solution_test.go
@@ -14,18 +14,46 @@ type ListNode struct {
 }
 
 // 本质上找到递归结束的条件 子问题的定义
+//func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+//	if list1 == nil {
+//		return list2
+//	}
+//	if list2 == nil {
+//		return list1
+//	}
+//	if list1.Val < list2.Val {
+//		list1.Next = mergeTwoLists(list1.Next, list2)
+//		return list1
+//	} else {
+//		list2.Next = mergeTwoLists(list1, list2.Next)
+//		return list2
+//	}
+//}
+
+// 非递归方式
 func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	head := &ListNode{
+		Val:  0,
+		Next: nil,
+	}
+	cur := head
+	for list1 != nil && list2 != nil {
+		if list1.Val < list2.Val {
+			cur.Next = list1
+			list1 = list1.Next
+			cur = cur.Next
+		} else {
+			cur.Next = list2
+			list2 = list2.Next
+			cur = cur.Next
+		}
+	}
+
 	if list1 == nil {
-		return list2
-	}
-	if list2 == nil {
-		return list1
-	}
-	if list1.Val < list2.Val {
-		list1.Next = mergeTwoLists(list1.Next, list2)
-		return list1
+		cur.Next = list2
 	} else {
-		list2.Next = mergeTwoLists(list1, list2.Next)
-		return list2
+		cur.Next = list1
 	}
+
+	return head.Next
 }
