28a4e3ecd2e08320d7f08a2d3e7a9417c7f2be5b
xiaobinfeng <rgfxb@qq.com>
2022-06-03 23:33:02 +0800

    info=add 206

---
+++ reverse_list_206.go
@@ -0,0 +1,27 @@
+package LeetCode
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+// 理解定义pre节点
+func reverseList(head *ListNode) *ListNode {
+	var pre *ListNode
+	cur := head
+	for cur != nil {
+		next := cur.Next
+		cur.Next = pre
+		pre = cur
+		cur = next
+	}
+	return pre
+}
