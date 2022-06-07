c220370bc2c8c2b8d852fc436755d0ca6c238aff
xiaobinfeng <rgfxb@qq.com>
2022-06-07 23:03:39 +0800

    info=add 234

--- is_palindrome_234/solution_test.go
+++ is_palindrome_234/solution_test.go
@@ -9,53 +9,55 @@ package is_palindrome_234
  */
 
 type ListNode struct {
-  Val  int
-  Next *ListNode
+	Val  int
+	Next *ListNode
 }
 
+// todo 提取方法
+
 func isPalindrome(head *ListNode) bool {
-  if head.Next == nil {
-    return true
-  }
-  // 先找到中间节点mid
-  slow, fast := head, head
-  for fast != nil && fast.Next != nil {
-    slow = slow.Next
-    fast = fast.Next.Next
-  }
+	if head.Next == nil {
+		return true
+	}
+	// 先找到中间节点mid
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
 
-  var mid *ListNode
-  var odd bool
-  if fast == nil {
-    mid = slow
-    odd = false
-  } else {
-    mid = slow.Next
-    odd = true
-  }
+	var mid *ListNode
+	var odd bool
+	if fast == nil {
+		mid = slow
+		odd = false
+	} else {
+		mid = slow.Next
+		odd = true
+	}
 
-  // 翻转前半部分
-  var pre *ListNode
-  cur := head
-  for cur != mid {
-    next := cur.Next
-    cur.Next = pre
-    pre = cur
-    cur = next
-  }
+	// 翻转前半部分
+	var pre *ListNode
+	cur := head
+	for cur != mid {
+		next := cur.Next
+		cur.Next = pre
+		pre = cur
+		cur = next
+	}
 
-  cur = pre
-  // 双指针 中间往两边遍历
-  if odd {
-    cur = cur.Next
-  }
-  // 注意结束条件
-  for cur != nil {
-    if cur.Val != mid.Val {
-      return false
-    }
-    cur = cur.Next
-    mid = mid.Next
-  }
-  return true
+	cur = pre
+	// 双指针 中间往两边遍历
+	if odd {
+		cur = cur.Next
+	}
+	// 注意结束条件
+	for cur != nil {
+		if cur.Val != mid.Val {
+			return false
+		}
+		cur = cur.Next
+		mid = mid.Next
+	}
+	return true
 }
