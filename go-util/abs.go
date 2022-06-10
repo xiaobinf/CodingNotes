07e2dcf0b5d60db177787d9dfa2de95bba750c89
xiaobinfeng <rgfxb@qq.com>
2022-06-10 23:10:49 +0800

    info=定义绝对值函数

---
+++ go-util/abs.go
@@ -0,0 +1,29 @@
+package go_util
+
+func AbsInt8(x int8) int8 {
+  if x < 0 {
+    return -x
+  }
+  return x
+}
+
+func AbsInt16(x int16) int16 {
+  if x < 0 {
+    return -x
+  }
+  return x
+}
+
+func AbsInt32(x int32) int32 {
+  if x < 0 {
+    return -x
+  }
+  return x
+}
+
+func AbsInt64(x int64) int64 {
+  if x < 0 {
+    return -x
+  }
+  return x
+}
