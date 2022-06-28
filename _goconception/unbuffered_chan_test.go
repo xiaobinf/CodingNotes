26316549af9a918575fc1023311460deb30c2bb4
xiaobinfeng <rgfxb@qq.com>
2022-06-28 22:49:46 +0800

    info=update

--- _goconception/unbuffered_chan_test.go
+++ _goconception/unbuffered_chan_test.go
@@ -1,60 +1,60 @@
 package _goconception
 
 import (
-  "fmt"
-  "math/rand"
-  "sync"
-  "testing"
-  "time"
+	"fmt"
+	"math/rand"
+	"sync"
+	"testing"
+	"time"
 )
 
 var (
-  wgp sync.WaitGroup
+	wgp sync.WaitGroup
 )
 
 // 参考Go语言实战
 func PlayCourt() {
-  wgp.Add(2)
-  court := make(chan int)
-  rand.Seed(time.Now().UnixNano())
-  go Runner("Nadal", court)
-  go Runner("Djokovic", court)
-  court <- 1
-  wgp.Wait()
+	wgp.Add(2)
+	court := make(chan int)
+	rand.Seed(time.Now().UnixNano())
+	go Player("Nadal", court)
+	go Player("Djokovic", court)
+	court <- 1
+	wgp.Wait()
 }
 
-func Runner(player string, court chan int) {
-  defer wgp.Done()
-  for {
-    ball, ok := <-court
-    if !ok {
-      fmt.Printf("%s win\n", player)
-      return
-    }
+func Player(player string, court chan int) {
+	defer wgp.Done()
+	for {
+		ball, ok := <-court
+		if !ok {
+			fmt.Printf("%s win\n", player)
+			return
+		}
 
-    // 随机判断是否可以接住
-    num := rand.Intn(100)
-    if num%7 == 0 {
-      fmt.Printf("%s defeat\n", player)
-      close(court)
-      return
-    }
+		// 随机判断是否可以接住
+		num := rand.Intn(100)
+		if num%7 == 0 {
+			fmt.Printf("%s defeat\n", player)
+			close(court)
+			return
+		}
 
-    fmt.Printf("%s hit ball %d\n", player, ball)
-    ball++
-    court <- ball
-  }
+		fmt.Printf("%s hit ball %d\n", player, ball)
+		ball++
+		court <- ball
+	}
 }
 
 func OverLoadChan() {
-  var m = make(chan int, 10)
-  for i := 0; i < 100; i++ {
-    m <- i
-    // 当写入的数据超过通道的长度，程序会被阻塞住
-  }
+	var m = make(chan int, 10)
+	for i := 0; i < 100; i++ {
+		m <- i
+		// 当写入的数据超过通道的长度，程序会被阻塞住
+	}
 }
 
 func TestChan(t *testing.T) {
-  PlayCourt()
-  //OverLoadChan()
+	PlayCourt()
+	//OverLoadChan()
 }
