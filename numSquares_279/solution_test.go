b863c796b304474474c2edb447be07c3632912fc
xiaobinf <rgfxb@qq.com>
2023-04-10 22:39:40 +0800

    --info=完全平方数

---
+++ numSquares_279/solution_test.go
@@ -0,0 +1,35 @@
+package numSquares_279
+
+import (
+	"fmt"
+	"testing"
+)
+
+/*这里使用动态规划来做。时间复杂度O(nlogn)，空间复杂度O(n)。代码非常精简
+
+定义一个函数f(n)表示我们要求的解。f(n)的求解过程为：
+f(n) = 1 + min{
+  f(n-1^2), f(n-2^2), f(n-3^2), f(n-4^2), ... , f(n-k^2) //(k为满足k^2<=n的最大的k)
+}
+*/
+
+func numSquares(n int) int {
+	var dp = make([]int, n+1)
+	dp[0], dp[1] = 0, 1
+	for i := 1; i <= n; i++ {
+		var MIN = 2147483647
+		for j := 1; j*j <= i; j++ {
+			if dp[i-j*j] < MIN {
+				MIN = dp[i-j*j]
+			}
+		}
+
+		dp[i] = MIN + 1
+	}
+
+	return dp[n]
+}
+
+func TestName(t *testing.T) {
+	fmt.Println(numSquares(2))
+}
