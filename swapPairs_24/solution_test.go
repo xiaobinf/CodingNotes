a2f1bee0af3cb3da264c533013236d561ca9a98b
xiaobinf <rgfxb@qq.com>
2023-04-19 21:50:57 +0800

    --other=update

---
+++ swapPairs_24/solution_test.go
@@ -0,0 +1,49 @@
+package swapPairs_24
+
+import "testing"
+
+/**
+* Definition for singly-linked list.
+* type ListNode struct {
+*     Val int
+*     Next *ListNode
+* }
+ */
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+// 两两交换链表的节点
+func swapPairs(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+
+	dummy := &ListNode{Next: head}
+	p := dummy
+	for p.Next != nil && p.Next.Next != nil {
+		p.Next.Next = p
+		p.Next = p.Next.Next
+		p = p.Next
+	}
+
+	return dummy
+}
+
+func swapPairs1(head *ListNode) *ListNode {
+	dummy := &ListNode{Next: head}
+	prev := dummy
+	for prev.Next != nil && prev.Next.Next != nil {
+		node1 := prev.Next
+		node2 := node1.Next
+		prev.Next, node1.Next, node2.Next = node2, node2.Next, node1
+		prev = node1
+	}
+	return dummy.Next
+}
+
+func TestName(t *testing.T) {
+
+}
