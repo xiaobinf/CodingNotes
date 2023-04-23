441f5e71c4922afdb39b6acb56956fe4a1dea509
xiaobinf <rgfxb@qq.com>
2023-04-23 22:58:44 +0800

    --info=update

---
+++ findKthLargest_215/solution_test.go
@@ -0,0 +1,15 @@
+package findKthLargest_215
+
+import (
+	"fmt"
+	"testing"
+)
+
+// findKthLargest 第k大的元素
+func findKthLargest(nums []int, k int) int {
+	return 0
+}
+
+func TestName(t *testing.T) {
+	fmt.Println(findKthLargest([]int{3, 4, 2}, 2))
+}
