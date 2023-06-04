866c37f5d3cddb487faf4a2c7a1efe0e2fae1ed2
xiaobinf <rgfxb@qq.com>
2023-06-04 16:19:46 +0800

    基础数据结构定义

--- _goconception/array_test.go
+++ _goconception/array_test.go
@@ -8,4 +8,10 @@ import (
 func TestArray(t *testing.T) {
 	c := "我是谁中wss"
 	fmt.Println(c, fmt.Sprintf("字符串长度为：%d", len(c)))
+
+	var str = "123中文"
+	fmt.Println(str[1], str[3:6])
+	fmt.Println([]rune(str[3:6])) // 一个中文占3个字节
+
+	fmt.Println(len([]rune("你好a")))
 }
