04d38e76e19ab07777381621014c35b5930fad3d
xiaobinf <rgfxb@qq.com>
2023-06-14 00:02:48 +0800

    最大子数组和

--- leetcode/max_sub_array_53/solution_test.go
+++ leetcode/max_sub_array_53/solution_test.go
@@ -15,21 +15,21 @@ func maxInt(a, b int) int {
 // 算法本质，当一个子序列逐个相加和小于0，说明该序列对后续的序列和的贡献为0，需要重新定义子序列的头部
 // 以序列和小于0为分界点，一次循环，处理完所有的的子问题。
 // 注意数组元素可能全部是负数,所以maxSum初始化为第一个元素的值
-func maxSubArray(nums []int) int {
-	var maxSum, tmpSum = nums[0], 0
-	for i := 0; i < len(nums); i++ {
-		// 每一步都是计算了当前的子序列的和
-		if tmpSum > 0 {
-			tmpSum = tmpSum + nums[i]
-		} else {
-			// 从头开始计算
-			tmpSum = nums[i]
-		}
-		// 每一步保存最大和
-		maxSum = maxInt(maxSum, tmpSum)
-	}
-	return maxSum
-}
+//func maxSubArray(nums []int) int {
+//	var maxSum, tmpSum = nums[0], 0
+//	for i := 0; i < len(nums); i++ {
+//		// 每一步都是计算了当前的子序列的和
+//		if tmpSum > 0 {
+//			tmpSum = tmpSum + nums[i]
+//		} else {
+//			// 从头开始计算
+//			tmpSum = nums[i]
+//		}
+//		// 每一步保存最大和
+//		maxSum = maxInt(maxSum, tmpSum)
+//	}
+//	return maxSum
+//}
 
 func TestName(t *testing.T) {
 	var arr0 = []int{-2, 1, -3, 4, -1, 2, 1, -5, 4}
@@ -42,5 +42,20 @@ func TestName(t *testing.T) {
 	assert.Equal(t, maxSubArray(arr2), 23)
 	assert.Equal(t, maxSubArray(arr3), -1)
 	assert.Equal(t, maxSubArray(arr4), 0)
+}
 
+func maxSubArray(nums []int) int {
+	var maxSum, tmpSum = nums[0], 0 //取nums[0]是为了避免全部是负数，本质是也是逐个比较
+	for i := 0; i < len(nums); i++ {
+		// 核心
+		if tmpSum > 0 {
+			tmpSum = tmpSum + nums[i]
+		} else {
+			tmpSum = nums[i]
+		}
+
+		maxSum = maxInt(maxSum, tmpSum)
+	}
+
+	return maxSum
 }
