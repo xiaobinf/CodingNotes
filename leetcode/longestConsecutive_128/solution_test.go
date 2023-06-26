73cddbc795f3edff834b28506dee10414fa23bd7
xiaobinf <rgfxb@qq.com>
2023-06-26 21:43:11 +0800

    复习

--- leetcode/longestConsecutive_128/solution_test.go
+++ leetcode/longestConsecutive_128/solution_test.go
@@ -39,3 +39,31 @@ func TestName(t *testing.T) {
 	fmt.Println(longestConsecutive([]int{100, 4, 200, 1, 3, 2}))
 	fmt.Println(longestConsecutive([]int{100}))
 }
+
+
+// 复习 在数组中寻找最长连续序列
+func longestConsecutive(nums []int) int {
+    if len(nums)==0 {
+		return 0
+	}
+	m := make(map[int]bool)
+	maxLen := 1
+	for _, value := range nums {
+		m[value] = true
+	}
+
+	for num, _ := range m {
+		if !m[num-1]{
+			start := num
+			len := 1
+			for m[start+1] {
+				start++
+				len++ 
+			}
+			maxLen = max(maxLen, len)
+		}
+	}
+
+
+	return maxLen
+}
